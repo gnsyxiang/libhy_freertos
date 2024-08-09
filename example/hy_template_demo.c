3ae8815f717fa56837cf8bd604b6bf99b1fa4ffa
zhenquan.qiu <gnsyxiang@163.com>
2024-08-09 08:52:52 +0800

    refactor: refactor hy_template_demo.c

--- example/hy_template_demo.c
+++ example/hy_template_demo.c
@@ -28,29 +28,33 @@
 #define __FILENAME__ (strrchr(__FILE__, '/') ? (strrchr(__FILE__, '/') + 1):__FILE__)
 #define os_printf(_fmt_, ...) printf("[log ] %s:%d | "_fmt_"\r\n", __FILENAME__, __LINE__, ##__VA_ARGS__)
 
-static TaskHandle_t xTask_creat = NULL;
+#define _TASK_CNT   (4)
 
-static void creat_task(void *p)
+typedef struct {
+    TaskHandle_t    task_h;
+    char            name[16];
+} _task_s;
+
+static _task_s task_h[_TASK_CNT];
+
+static void _task_loop_cb(void *args)
 {
-    os_printf("%s", __FUNCTION__);
-
     hy_s32_t cnt = 0;
-
-    taskENTER_CRITICAL();
-    taskEXIT_CRITICAL();
+    _task_s *task = (_task_s *)args;
 
     while (1) {
-        os_printf("this is creat task:idle-%d", cnt++);
+        os_printf("this is %s, idle-%d", task->name, cnt++);
+
         vTaskDelay(1000);
 
-        if (cnt >= 10) {
+        if (cnt >= 5) {
             break;
         }
     }
 
-    os_printf("delete creat task");
+    os_printf("delete %s", task->name);
 
-    vTaskDelete(xTask_creat);
+    vTaskDelete(task->task_h);
 }
 
 int main(void)
@@ -59,20 +63,27 @@ int main(void)
 
     os_printf("Freertos v10.2.1 start ");
 
-    ret = xTaskCreate((TaskFunction_t )creat_task,
-                      (const char *   )"creat_task",
-                      (unsigned short )128,
-                      (void *         )NULL,
-                      (UBaseType_t    )1,
-                      (TaskHandle_t * )&xTask_creat);
-    if (pdPASS != ret) {
-        return -1;
+    taskENTER_CRITICAL();
+    taskEXIT_CRITICAL();
+
+    for (hy_s32_t i = 0; i < _TASK_CNT; i++) {
+        _task_s *task = &task_h[i];
+        memset(task, 0, sizeof(*task));
+        snprintf(task->name, sizeof(task->name), "task%d", i);
+
+        ret = xTaskCreate(_task_loop_cb, task->name, 128, task, 1, &task->task_h);
+        if (pdPASS != ret) {
+            return -1;
+        }
     }
 
     vTaskStartScheduler();
 
+    os_printf("-1-here");
     while (1) {
+        os_printf("-2-here");
     }
+    os_printf("-3-here");
 
     return 0;
 }
